93dea6d7285aaa28934f4229150d4054c8fe827f
sanadoing <143790326+sanadoing@users.noreply.github.com>
2024-02-05 16:17:11 +0900

    [24.02.05] 2741번 풀이

---
+++ bronze/B_2741.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class B_2741 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		for (int i = 0; i < N; i++) {
+			System.out.println(i + 1);
+		}
+
+	}
+
+}
