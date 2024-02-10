6d86fda0060e15d35d12b997080bd258df1799a8
sanadoing <mountrive33@gmail.com>
2024-02-10 21:05:50 +0900

    [24.02.10] solved class 2+ 풀이

--- bronze/B_2231.java
+++ bronze/B_2231.java
@@ -7,23 +7,18 @@ public class B_2231 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		int temp, generator;
-		int ten;
-		
+		String str;
+		int result;
 		for (int i = 1; i < N; i++) {
-			temp = i;
-			generator = i;
-			ten = 1;
-			while(temp<ten) {
-				generator+= (temp%ten);
-				System.out.print("i "+i +"ten "+ten+"i/ten " + (i/ten));
-				ten *= 10;
-				
-			}
-			generator+= i%10;
-			System.out.print(" " + i%10+"\n");
-			System.out.println(i +" === " + generator);
-
+			str = i + "";
+      result = i;
+      for(int j = 0; j<str.length();j++){
+        result += str.charAt(j) - '0';
+      }
+      if(result == N){
+        System.out.println(i);
+        break;
+      }
 		}
 	}
 }
