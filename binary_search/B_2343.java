f63ec1a09cc75f91a762a4297047394d6b35f91d
sanadoing <143790326+sanadoing@users.noreply.github.com>
2024-02-16 14:49:43 +0900

    [24.02.16] 2343번 풀이

--- binary_search/B_2343.java
+++ binary_search/B_2343.java
@@ -1,7 +1,61 @@
 package binary_search;
 
 public class B_2343 {
-  public static void main(String[] args) {
-    
-  }
+	static int N, M;
+	static int sum, max;
+	static int[] lectures;
+
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		lectures = new int[N];
+		sum = 0;
+		max = 0;
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			lectures[i] = Integer.parseInt(st.nextToken());
+			sum += lectures[i];
+			max = Math.max(max, lectures[i]);
+		}
+
+		int left = max;
+		int right = sum - max;
+		int mid = (left + right) / 2;
+
+		while (left <= right) {
+			mid = (left + right) / 2;
+
+			if (check(mid) <= M) {
+				right = mid - 1;
+
+			} else {
+				left = mid + 1;
+
+			}
+		}
+		System.out.println(left);
+	}
+
+	static int check(int m) {
+		int count = 0;
+		int sum = 0;
+
+		for (int i = 0; i < N; i++) {
+			if (sum + lectures[i] > m) {
+				count++;
+				sum = 0;
+			}
+			sum += lectures[i];
+
+		}
+		if (sum >= 0) {
+			count++;
+		}
+
+		return count;
+	}
 }
