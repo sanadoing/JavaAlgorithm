22e7645129da18894bf5f2dea13ccb6d86fce5b7
sanadoing <mountrive33@gmail.com>
2024-03-22 23:33:06 +0900

    [24.03.22] 브론즈 풀이

---
+++ B_2744.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class B_2744 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String str = sc.next();
+    for (int i = 0; i < str.length(); i++) {
+      char c = str.charAt(i);
+      if (Character.isLowerCase(c)) {
+        c = Character.toUpperCase(c);
+      } else if (Character.isUpperCase(c))
+        c = Character.toLowerCase(c);
+      System.out.print(c);
+    }
+  }
+}
