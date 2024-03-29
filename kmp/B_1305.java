906f86d3d300241d776771fc8706d314301ab256
sanadoing <143790326+sanadoing@users.noreply.github.com>
2024-03-29 17:06:07 +0900

    [24.01305.java

---
+++ kmp/B_1305.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		String[] input = br.readLine().split("");
+		int[] pi = new int[N];
+
+		int j = 0;
+		for (int i = 1; i < input.length; i++) {
+			while (j > 0 && !input[i].equals(input[j]))
+				j = pi[j - 1];
+			if (input[i].equals(input[j]))
+				pi[i] = ++j;
+
+		}
+		System.out.println(N - 1 - pi[N - 1] + 1);
+	}
+}
