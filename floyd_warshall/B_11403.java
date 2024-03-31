c4cde267321aa7f5f2cd5b67aa2305b98a4e126b
sanadoing <mountrive33@gmail.com>
2024-03-31 21:24:03 +0900

    [24.03.31] 11403 (실1) 경로 찾기 풀이

---
+++ floyd_warshall/B_11403.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B_11403 {
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
+    int[][] board = new int[N][N];
+    StringTokenizer st;
+    for (int i = 0; i < N; i++) {
+      st = new StringTokenizer(br.readLine());
+      for (int j = 0; j < N; j++) {
+        board[i][j] = Integer.parseInt(st.nextToken());
+      }
+    }
+
+    for (int k = 0; k < N; k++) {
+      for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+          if (board[i][k] == 1 && board[k][j] == 1) {
+            board[i][j] = 1;
+          }
+        }
+      }
+
+    }
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++) {
+        sb.append(board[i][j]).append(" ");
+      }
+      sb.append("\n");
+    }
+    System.out.println(sb);
+  }
+}
