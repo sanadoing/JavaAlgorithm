4a81f0e37c848911f8f97f0b08049d50ddefe46b
sanadoing <mountrive33@gmail.com>
2024-04-06 22:56:50 +0900

    [24.04.06] bronze 풀이

---
+++ bronze/B_15680.java
@@ -0,0 +1,16 @@
+package bronze;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class B_15680 {
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
+    if (N == 0) {
+      System.out.println("YONSEI");
+    } else {
+      System.out.println("Leading the Way to the Future");
+    }
+  }
+}
