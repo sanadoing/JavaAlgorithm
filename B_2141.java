ab0ab2e3fb7bbcb8de4a37c379e7f268015ac201
sanadoing <mountrive33@gmail.com>
2024-05-05 23:49:22 +0900

    [24.05.05] bronze 풀이

--- B_2141.java
+++ B_2141.java
@@ -1,24 +1,35 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
 
+// 89% 에서 틀림 -> sum 이 짝수 일 때 중앙 값 : sum /2 , 홀수 일 때 중앙 값 : (sum+1) / 2
 public class B_2141 {
   public static void main(String[] args) throws Exception {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringTokenizer st;
     int N = Integer.parseInt(br.readLine());
-    int[] people = new int[N];
-    int sum = 0;
+    Long[] people = new Long[N];
+    Map<Long, Long> map = new HashMap<>();
+    Long sum = (long) 0;
     for (int i = 0; i < N; i++) {
       st = new StringTokenizer(br.readLine());
-      int a = Integer.parseInt(st.nextToken());
-      int b = Integer.parseInt(st.nextToken());
-      people[i] = b;
-      sum += people[i];
+      Long a = Long.parseLong(st.nextToken());
+      Long b = Long.parseLong(st.nextToken());
+      people[i] = a;
+      map.put(a, b);
+      sum += b;
     }
-
+    Arrays.sort(people);
+    Long left_sum = (long) 0;
     for (int i = 0; i < N; i++) {
-
+      left_sum += map.get(people[i]);
+      if (left_sum >= (sum + 1) / 2) {
+        System.out.println(people[i]);
+        break;
+      }
     }
   }
 }
---
+++ B_2440.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class B_2440 {
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
+    for (int i = N; i > 0; i--) {
+      for (int j = 0; j < i; j++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+}
