5efa3466e212960f317eb4c04e21c7d62c62d566
sanadoing <mountrive33@gmail.com>
2024-05-05 23:52:34 +0900

    [24.05.05] 파일 이동

--- implement/B_18500.java
+++ implement/B_18500.java
@@ -1,3 +1,5 @@
+package implement;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
--- mathematics/B_1963.java
+++ mathematics/B_1963.java
@@ -1,3 +1,5 @@
+package mathematics;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
