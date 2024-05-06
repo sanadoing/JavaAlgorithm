a7abc087bbc693f1ce9e80b437dd222910c32644
sanadoing <mountrive33@gmail.com>
2024-05-06 23:42:19 +0900

    [24.05.06] bronze 풀이

---
+++ bronze/B_2742.java
@@ -0,0 +1,13 @@
+package bronze;
+
+import java.util.Scanner;
+
+public class B_2742 {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int N = scan.nextInt();
+    for (int i = N; i > 0; i--) {
+      System.out.println(i);
+    }
+  }
+}
